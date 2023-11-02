152e7f5f910c57119edb9b0664912b7fecd2d811
MikeySweetman <sweetmanmikey@gmail.com>
2023-11-02 11:47:27 +0000

    finished part 1 of assignment

--- assignment1/part1/matrix.c
+++ assignment1/part1/matrix.c
@@ -11,11 +11,15 @@ int main(void){
 	/*Here is the block of code that 
     creates the intial 4x4 matrix with
     the required values */
+
+	//in this code I use i as row index and j as colum index
 	int i, j;
-	double matrix[4][4];// = {};
+	double matrix[4][4];
 	
+
 	i = 0;
 	j = 0; 
+
 	
 	for(i=0;i<4;i++){
 		for(j=0;j<4;j++){
@@ -24,24 +28,27 @@ int main(void){
 		}
 	}
 	
-	//printing the matrix
+
+	//printing the 4x4 matrix
 	for(i=0;i<4;i++){
 		printf("%f  %f  %f  %f\n",matrix[i][0], matrix[i][1], matrix[i][2], matrix[i][3]);
 	}
+
 	
 	/*calling the function that calculates 
     the determinant of a 3x3 matrix */
     double det4;
 	det4 =  cramers(matrix);
-	printf("\nthe det of the 4x4 matrix is %f\n", det4);	
+	printf("\nthe determinant of the 4x4 matrix is %f\n", det4);	
 
 
 
 
-	return 0;
+  return 0;
 }
 
-
+//=====================================================================================
+//functions below
 
 
 //function to calculate the determinant of a 3x3 matrix
@@ -62,22 +69,29 @@ double det3(double matrix[3][3]){
 
 
 /*function to split 4x4 matrix into 3x3 matricies,
- * the dets of which will be calculated*/
+ * the dets of which will be calculated
+ * using the function det3
+ *
+ * I am creating four submatricies given in the
+ * assignment document, index counters
+ * i and j correspond to the position in
+ * the 4x4 matrix
+ * eg "g" is at i = 1, j= 2 in 4x4 matrix
+ */
 double cramers(double matrix[4][4]){
-	//declaring the 4 submatricies and coeffcienets
-	//this need to multiply the det of 3x3 by this so possibly
-	//dont actually need these here, can call in the main
+	//coeffecient to multiply 3x3 dets by
 	double a = matrix[0][0];
 	double b = matrix[0][1];
 	double c = matrix[0][2];
 	double d = matrix[0][3];
 	
+	//named 3x3s after the coefficent they're mulitplied by
 	double amat[3][3], bmat[3][3], cmat[3][3], dmat[3][3];
 	
 	int i = 0; int j = 0;
+	
+
 	//filling the submatricies
-	//in all of these i and j correspond to the 4x4 indicies
-	//these al have to map to different 3x3 indices 
 	//amatrix
 	for(i=1;i<4;i++){
 		for(j=1;j<4;j++){		
@@ -86,6 +100,18 @@ double cramers(double matrix[4][4]){
 	}
 	
 
+	/*
+     *loops for filling the b and c matricies are
+	 * a little bit ugly as the indicies between the 
+	 * 4x4 and 3x3 matricies don't map nicely
+	 * 
+	 * eg for cmat 4x4 colum indices 0 1 3 has 
+	 * to go to 0 1 2 in the 3x3 matrix
+	 * for each row
+	 *
+	 * bmat is 0 2 3 to 0 1 2 for
+	 * j index 
+     */ 
 	//bmat
 	for(i=1;i<4;i++){
 		for(j=1;j<4;j++){
@@ -122,17 +148,18 @@ double cramers(double matrix[4][4]){
 		}
 	}
 
-/*now that I have the four matricies required I can 
-calulate the determinatnes of them and get the final determinant
-*/
-double final_det;
+	/*
+     * now that I have the four matricies required I can 
+     * calulate the determinants of them and get the final determinant
+	 */
+	double final_det;
 
-final_det = a * det3(amat)
+	final_det = a * det3(amat)
 			- b * det3(bmat)
 			+ c * det3(cmat)
 			- d * det3(dmat);
 
-return final_det;
+  return final_det;
 
 }
 
